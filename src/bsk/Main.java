4bae3e81924165914371a0383f371a25e86ca35b
hatul <p.piatek97@gmail.com>
2019-03-10 19:59:37 +0100

    Zadanie 3 - 2c dekodowanie działa :)_

--- src/bsk/Main.java
+++ src/bsk/Main.java
@@ -1,28 +1,17 @@
 package bsk;
 
-import java.io.*;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 
-//        String inputString = "CRYPTOGRAPHY";
-//        String key = "CONVENIENCE";
+        //Zadanko1.saveToFile();
+        //Zadanko2.saveToFile();
+        //Zadanko3.saveToFile();
+        //Zadanko4.saveToFile();
 
-//        String inputString = "ALAMAKOTA";
-//        String key = "ALA";
-//
-//        String outputString;
-//        outputString = Zadanko5.encode(inputString, key);
-//
-//        System.out.println(outputString);
+        Zadanko3.saveToFile();
+        System.out.println(Zadanko3.decode2C("HEESPNI_ANTRRSSEESIDEIYDASCBTEEMGEPNNEEANDIHDCTARTAHSOAVOHIEERODARMEOCYYFTTKGEENPURSAJEIRSCIHNCEVGESOSTKTAEAHFID", "CONVENIENCE"));
 
-//     Zadanko1.saveToFile();
-    //Zadanko2.saveToFile();
-    //Zadanko3.saveToFile();
-    //Zadanko4.saveToFile();
-//    Zadanko5.saveToFile();
         Zadanko1.saveDecodeToFile();
     }
 }
--- src/bsk/Zadanko3.java
+++ src/bsk/Zadanko3.java
@@ -2,7 +2,10 @@ package bsk;
 
 
 import java.io.*;
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class Zadanko3 {
 
@@ -60,10 +63,8 @@ public class Zadanko3 {
 
         Integer liczba_znakow_w_bloku = ((1 + key.length()) * key.length()) / 2;
         Integer liczba_blokow = inputString.length() / liczba_znakow_w_bloku + 1;
-        Integer liczba_znakow_w_ostatnim_bloku = -1;
-        if (liczba_blokow > 1) liczba_znakow_w_ostatnim_bloku = inputString.length() % liczba_znakow_w_bloku;
-        Boolean[][] blok_przykladowy = new Boolean[key.length()][key.length()];
 
+        Boolean[][] blok_przykladowy = new Boolean[key.length()][key.length()];
         for (int i = 0; i < blok_przykladowy.length; i++) {
             for (int j = 0; j < blok_przykladowy.length; j++) {
                 blok_przykladowy[i][j] = false;
@@ -93,6 +94,7 @@ public class Zadanko3 {
             }
         }
 
+
         pozycja_w_napisie = 0;
         for (Character[][] blok : bloki_znakow) {
             for (int i = 0; i < code.size(); i++) {
@@ -103,9 +105,7 @@ public class Zadanko3 {
                         outputString += blok[j][kolumna];
                         pozycja_w_napisie++;
                     }
-
                 }
-
             }
         }
 
@@ -113,11 +113,96 @@ public class Zadanko3 {
         return outputString;
     }
 
-    public static void saveToFile(){
-        String key="";
-        String inputString="";
-        String outputString="";
-        String outputString2="";
+    public static String decode2C(String inputString, String key) {
+        String outputString = new String();
+        Map<Integer, Integer> code = sequenceFromKey(key);
+
+        Integer liczba_znakow_w_bloku = ((1 + key.length()) * key.length()) / 2;
+        Integer liczba_blokow = inputString.length() / liczba_znakow_w_bloku + 1;
+        Integer liczba_znakow = inputString.length();
+        Integer liczba_znakow_w_ostatnim_bloku = inputString.length() % liczba_znakow_w_bloku;
+
+        Boolean[][] blok_przykladowy = new Boolean[key.length()][key.length()];
+        for (int i = 0; i < blok_przykladowy.length; i++) {
+            for (int j = 0; j < blok_przykladowy.length; j++) {
+                blok_przykladowy[i][j] = false;
+            }
+        }
+        int liczba_uzytych_znakow = 0;
+        for (int i = 0; i < blok_przykladowy.length; i++) {
+            for (int j = 0; j <= code.get(i); j++) {
+                if (liczba_uzytych_znakow >= liczba_znakow) break;
+                blok_przykladowy[i][j] = true;
+                liczba_uzytych_znakow++;
+            }
+        }
+        Boolean[][] ostatni_blok_przykladowy = new Boolean[key.length()][key.length()];
+        for (int i = 0; i < ostatni_blok_przykladowy.length; i++) {
+            for (int j = 0; j < ostatni_blok_przykladowy.length; j++) {
+                ostatni_blok_przykladowy[i][j] = false;
+            }
+        }
+
+        if (liczba_blokow > 1) {
+            int liczba_uzytych_znakow_w_ostatnim_bloku = 0;
+            for (int i = 0; i < blok_przykladowy.length; i++) {
+                for (int j = 0; j <= code.get(i); j++) {
+                    if (liczba_uzytych_znakow_w_ostatnim_bloku >= liczba_znakow_w_ostatnim_bloku) break;
+                    ostatni_blok_przykladowy[i][j] = true;
+                    liczba_uzytych_znakow_w_ostatnim_bloku++;
+                }
+            }
+        }
+
+        List<Character[][]> bloki_znakow = new LinkedList<>();
+        for (int i = 0; i < liczba_blokow; i++)
+            bloki_znakow.add(new Character[key.length()][key.length()]);
+
+
+        int pozycja_w_napisie = 0;
+        for (Character[][] blok : bloki_znakow) {
+
+            if (blok == bloki_znakow.get(bloki_znakow.size() - 1) && liczba_blokow > 1) {
+                for (int i = 0; i < blok_przykladowy.length; i++) {
+                    int kolumna = code.get(i);
+                    for (int j = 0; j < blok_przykladowy.length; j++) {
+                        if (pozycja_w_napisie >= inputString.length()) break;
+                        if (ostatni_blok_przykladowy[j][kolumna] == true) {
+                            blok[j][kolumna] = inputString.charAt(pozycja_w_napisie);
+                            pozycja_w_napisie++;
+                        }
+                    }
+                }
+            } else {
+                for (int i = 0; i < blok_przykladowy.length; i++) {
+                    int kolumna = code.get(i);
+                    for (int j = 0; j < blok_przykladowy.length; j++) {
+                        if (pozycja_w_napisie >= inputString.length()) break;
+                        if (blok_przykladowy[j][kolumna] == true) {
+                            blok[j][kolumna] = inputString.charAt(pozycja_w_napisie);
+                            pozycja_w_napisie++;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (Character[][] blok : bloki_znakow) {
+            for (int i = 0; i < blok_przykladowy.length; i++) {
+                for (int j = 0; j < blok_przykladowy.length; j++) {
+                    if (blok[i][j] != null) outputString += blok[i][j];
+                }
+            }
+        }
+
+        return outputString;
+    }
+
+    public static void saveToFile() {
+        String key = "";
+        String inputString = "";
+        String outputString = "";
+        String outputString2 = "";
 
         File inputFile = new File("src/bsk/results/In03.txt");
         File outputFile = new File("src/bsk/results/Out03.txt");
@@ -127,14 +212,14 @@ public class Zadanko3 {
         try {
             bufferedReader = new BufferedReader(new FileReader(inputFile));
             key = bufferedReader.readLine();
-            inputString=bufferedReader.readLine();
+            inputString = bufferedReader.readLine();
         } catch (FileNotFoundException e) {
-            System.out.println("Error with file: "+ e.getMessage());
+            System.out.println("Error with file: " + e.getMessage());
         } catch (IOException e) {
-            System.out.println("Error with file: "+ e.getMessage());
+            System.out.println("Error with file: " + e.getMessage());
         }
-        outputString=encode2B(inputString, key);
-        outputString2=encode2C(inputString,key);
+        outputString = encode2B(inputString, key);
+        outputString2 = encode2C(inputString, key);
 
         try {
             FileWriter fileWriter = new FileWriter(outputFile);
@@ -144,9 +229,9 @@ public class Zadanko3 {
             fileWriter.close();
             fileWriter2.close();
         } catch (FileNotFoundException e) {
-            System.out.println("Error with file: "+ e.getMessage());
+            System.out.println("Error with file: " + e.getMessage());
         } catch (IOException e) {
-            System.out.println("Error with file: "+ e.getMessage());
+            System.out.println("Error with file: " + e.getMessage());
         }
 
     }
