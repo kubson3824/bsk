0f495689a6c0491ea243de0854e213464302b08d
Przemysław Piątek <p.piatek97@gmail.com>
2019-03-19 18:12:36 +0100

    kolejne psy xd

--- PS4-5/src/main/java/Zad1.java
+++ PS4-5/src/main/java/Zad1.java
@@ -1,3 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Zad1 {
 
+    public static List<Boolean[]> zad1(Boolean[] wielomian, Boolean[] seed, Integer n) {
+        List<Boolean[]> wynik = new ArrayList<>();
+        return wynik;
+    }
 }
