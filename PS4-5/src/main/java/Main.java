264586e618a8d5827ea27990336527efefd41d06
hatul <p.piatek97@gmail.com>
2019-03-24 16:13:08 +0100

    KOLEJNE PSY 1 zadanie wygląda że działa

--- PS4-5/src/main/java/Main.java
+++ PS4-5/src/main/java/Main.java
@@ -1,6 +1,14 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Boolean[]> booleans = Zad1.zad1(new Boolean[]{true, false, true}, new Boolean[]{true, false, false}, 20);//wielomiax x3 + x1 +1 , seed = 100
+        for (Boolean[] aBoolean : booleans) {
+            for (Boolean aBoolean1 : aBoolean) {
+                System.out.print(aBoolean1 + " ");
+            }
+            System.out.println();
+        }
         return;
     }
 }
--- PS4-5/src/main/java/Zad1.java
+++ PS4-5/src/main/java/Zad1.java
@@ -4,7 +4,40 @@ import java.util.List;
 public class Zad1 {
 
     public static List<Boolean[]> zad1(Boolean[] wielomian, Boolean[] seed, Integer n) {
+        //wielomian true false true, seed true false false
         List<Boolean[]> wynik = new ArrayList<>();
+
+        Boolean[] pierwszy = new Boolean[seed.length];
+        int liczba_jedynek = 0;
+        for (int i = 0; i < seed.length; i++)
+            if (wielomian[i])
+                if (seed[i])
+                    liczba_jedynek++;
+
+
+        for (int i = 0; i < seed.length - 1; i++) {
+            pierwszy[i + 1] = seed[i];
+        }
+        pierwszy[0] = (liczba_jedynek % 2) == 1;
+        liczba_jedynek = 0;
+        wynik.add(pierwszy);
+
+
+        for (int i = 0; i < n; i++) {
+            Boolean[] tab = new Boolean[seed.length];
+            for (int j = 0; j < seed.length; j++)
+                if (wielomian[j])
+                    if (wynik.get(i)[j])
+                        liczba_jedynek++;
+
+            for (int j = 0; j < seed.length - 1; j++) {
+                tab[j + 1] = wynik.get(i)[j];
+            }
+            tab[0] = (liczba_jedynek % 2) == 1;
+            liczba_jedynek = 0;
+            wynik.add(tab);
+        }
+
         return wynik;
     }
 }
