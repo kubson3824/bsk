5e47da6b7ca95060bf06f2c9c86e54cacebe8c04
hatul <p.piatek97@gmail.com>
2019-03-24 17:34:44 +0100

    Zad 3 całe

--- PS4-5/src/main/java/Main.java
+++ PS4-5/src/main/java/Main.java
@@ -2,7 +2,12 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Boolean[]> booleans = Zad1.zad1(new Boolean[]{true, false, true}, new Boolean[]{true, false, false}, 20);//wielomiax x3 + x1 +1 , seed = 100
+        Boolean[] wielomian = new Boolean[]{true, false, true};
+        Boolean[] seed = new Boolean[]{true, false, false};
+        Boolean[] message = new Boolean[]{true, false, false, false};
+
+
+        List<Boolean[]> booleans = Zad1.zad1(wielomian, seed, 20);//wielomiax x3 + x1 +1 , seed = 100
 //        for (Boolean[] aBoolean : booleans) {
 //            for (Boolean aBoolean1 : aBoolean) {
 //                System.out.print(aBoolean1 + " ");
@@ -10,16 +15,27 @@ public class Main {
 //            System.out.println();
 //        }
 
-        Boolean[] zad2_szyfruj = Zad2.zad2_szyfruj(new Boolean[]{true, false, true}, new Boolean[]{true, false, false}, new Boolean[]{true, false, false, false});
-        for (Boolean aBoolean : zad2_szyfruj) {
+        Boolean[] zad2_szyfruj = Zad2.zad2_szyfruj(wielomian, seed, message);
+//        for (Boolean aBoolean : zad2_szyfruj) {
+//            System.out.print(aBoolean + " ");
+//        }
+//        System.out.println();
+
+        Boolean[] zad2_deszyfruj = Zad2.zad2_deszyfruj(wielomian, seed, zad2_szyfruj);
+//        for (Boolean aBoolean : zad2_deszyfruj) {
+//            System.out.print(aBoolean + " ");
+//        }
+        Boolean[] zad3_szyfruj = Zad3.zad3_szyfruj(new Boolean[]{true, true, false, true}, new Boolean[]{true, false, false, true}, message);
+        for (Boolean aBoolean : zad3_szyfruj) {
             System.out.print(aBoolean + " ");
         }
         System.out.println();
 
-        Boolean[] zad2_deszyfruj = Zad2.zad2_deszyfruj(new Boolean[]{true, false, true}, new Boolean[]{true, false, false}, zad2_szyfruj);
-        for (Boolean aBoolean : zad2_deszyfruj) {
+        Boolean[] zad3_deszyfruj = Zad3.zad3_deszyfruj(new Boolean[]{true, true, false, true}, new Boolean[]{true, false, false, true}, zad3_szyfruj);
+        for (Boolean aBoolean : zad3_deszyfruj) {
             System.out.print(aBoolean + " ");
         }
+        System.out.println();
 
         return;
     }
---
+++ PS4-5/src/main/java/Zad3.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zad3 {
+
+    public static Boolean[] zad3_szyfruj(Boolean[] wielomian, Boolean[] seed, Boolean[] message) {
+        List<Boolean[]> wynik = new ArrayList<>();
+
+        Boolean[] pierwszy = new Boolean[seed.length];
+        int liczba_jedynek = 0;
+        for (int i = 0; i < seed.length; i++)
+            if (wielomian[i])
+                if (seed[i])
+                    liczba_jedynek++;
+
+
+        for (int i = 0; i < seed.length - 1; i++) {
+            pierwszy[i + 1] = seed[i];
+        }
+        if (message[0]) liczba_jedynek++;
+        pierwszy[0] = (liczba_jedynek % 2) == 1;
+        liczba_jedynek = 0;
+        wynik.add(pierwszy);
+//        System.out.println(pierwszy[0] + " " + pierwszy[1] + " " + pierwszy[2]);
+//        System.out.println();
+        for (int i = 0; i < message.length; i++) {
+            Boolean[] tab = new Boolean[seed.length];
+            for (int j = 0; j < seed.length; j++)
+                if (wielomian[j])
+                    if (wynik.get(i)[j])
+                        liczba_jedynek++;
+
+            for (int j = 0; j < seed.length - 1; j++) {
+                tab[j + 1] = wynik.get(i)[j];
+            }
+            if (i != message.length - 1)
+                if (message[i + 1]) liczba_jedynek++;
+            tab[0] = (liczba_jedynek % 2) == 1;
+            liczba_jedynek = 0;
+            wynik.add(tab);
+//            System.out.println(tab[0] + " " + tab[1] + " " + tab[2]);
+        }
+
+        Boolean[] zaszyfrowana = new Boolean[message.length];
+
+        for (int i = 0; i < message.length; i++) {
+            zaszyfrowana[i] = wynik.get(i)[0];
+        }
+
+
+        return zaszyfrowana;
+    }
+
+    public static Boolean[] zad3_deszyfruj(Boolean[] wielomian, Boolean[] seed, Boolean[] message) {
+        List<Boolean[]> wynik = new ArrayList<>();
+
+        Boolean[] pierwszy = new Boolean[seed.length];
+        pierwszy[0] = message[0];
+        for (int i = 0; i < seed.length - 1; i++) {
+            pierwszy[i + 1] = seed[i];
+        }
+        wynik.add(pierwszy);
+
+        for (int i = 0; i < message.length - 2; i++) {
+            Boolean[] tab = new Boolean[seed.length];
+            for (int j = 0; j < seed.length - 1; j++)
+                tab[j + 1] = wynik.get(i)[j];
+
+            if (i != message.length - 1)
+                tab[0] = message[i + 1];
+
+            wynik.add(tab);
+        }
+        wynik.add(0, seed);
+
+        Boolean[] zdeszyfrowana = new Boolean[message.length];
+        for (int i = 0; i < message.length; i++) {
+            int liczba_jedynek = 0;
+            for (int j = 0; j < seed.length; j++)
+                if (wielomian[j])
+                    if (wynik.get(i)[j])
+                        liczba_jedynek++;
+            if (message[i]) liczba_jedynek++;
+            zdeszyfrowana[i] = (liczba_jedynek % 2) == 1;
+
+        }
+
+
+        return zdeszyfrowana;
+    }
+}
