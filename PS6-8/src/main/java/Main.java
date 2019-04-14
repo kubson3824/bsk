dccc2469c8c4f6870b29af2c14c89682a672a2ca
Adam Polejczuk <adampolejczuk@gmail.com>
2019-04-14 20:14:23 +0200

    dekryptowanie

--- PS6-8/src/main/java/Main.java
+++ PS6-8/src/main/java/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String []args){
         BigInteger b = new BigInteger("133457799BBCDFF1", 16);
         BigInteger word = new BigInteger("0123456789ABCDEF", 16);
+        BigInteger wordToDecrypt = new BigInteger("85e813540f0ab405", 16);
         List<BigInteger> keys = DES.generateKeys(b);
         DES.encrypt(word, keys);
+        DES.decrypt(wordToDecrypt, keys);
 //        DES.cipherFunction(new BigInteger("11110000101010101111000010101010",2), new BigInteger("000110110000001011101111111111000111000001110010", 2));
     }
 }
